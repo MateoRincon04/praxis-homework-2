3907c84ce95738fe6d23395abab7c49063708e9a
MateoRincon04 <marinconar@unal.edu.co>
2021-11-03 10:41:36 -0500

    tool instalation and general setup

--- build-app/bootstrap.sh
+++ build-app/bootstrap.sh
@@ -1,2 +1,18 @@
 #!/usr/bin/env bash
 echo "Hello world!"
+
+# NECESSARY TOOLS TO EXECUTE THE PROYECT
+#Installations using yum and given a positive answer to posible permission requests
+
+# Git installation via yum command
+sudo yum install git -y
+
+# Go installation via yum command
+sudo yum install golang -y
+
+# npm & NodeJS installation commands
+curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
+sudo yum install nodejs -y
+
+# Vue cli installation command
+yes | npm install -g @vue/cli
