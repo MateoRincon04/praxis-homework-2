3e2a3c3346ffbe70221298873701ebb2e9e110c1
MateoRincon04 <marinconar@unal.edu.co>
2021-11-03 16:42:02 -0500

    solved the error, finished both back and front end environments and moved resulting files to shared folder

--- build-app/bootstrap.sh
+++ build-app/bootstrap.sh
@@ -14,24 +14,31 @@ go version
 # npm & NodeJS installation commands
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo bash -
 sudo yum install nodejs -y
-
 node --version
 npm --version
 
 # Vue cli installation command
-echo "aqui"
-# cd /
-# cd /home/vagrant/
-# touch package.json
-echo "removing package-lock"
-rm package-lock.json
-echo "removed package-lock"
-rm -rf node_modules
-echo "removing modules"
-npm config set registry https://registry.npmjs.org/
-echo "config registry"
-npm config set strict-ssl false
-echo "config strict-ssl"
 sudo npm install -g @vue/cli -y
+vue --version
 
-# Verify installation
+#Clone the test repository
+git clone https://github.com/jdmendozaa/vuego-demoapp.git
+
+#Build the environment for the back-end of the project that uses Go
+cd vuego-demoapp/server
+go build
+echo 'export PORT=4001' >> /etc/profile
+
+#Make a copy of the binary file called vuego-demoapp and move it to the shared folder
+cd /
+cp /home/vagrant/vuego-demoapp/server/vuego-demoapp /shared/
+
+# Building the environment fot the front-end of the project that uses Vue cli
+cd home/vagrant/vuego-demoapp/spa
+sudo npm install
+sudo echo 'VUE_APP_API_ENDPOINT="http://localhost:4001/api"' >> /home/vagrant/vuego-demoapp/spa/.env.production.local
+npm run build
+
+# Compress the generated folder dist.tar.gz and move it to the shared folder
+tar -zcvf dist.tar.gz ./dist
+mv dist.tar.gz /shared/
