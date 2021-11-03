861c3cd44569758cd7b7ee8fa6af1231eebddc22
MateoRincon04 <marinconar@unal.edu.co>
2021-11-03 12:24:40 -0500

    cloned test repo and built back-end env

--- build-app/bootstrap.sh
+++ build-app/bootstrap.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-echo "Hello world!"
 
 # NECESSARY TOOLS TO EXECUTE THE PROYECT
 #Installations using yum and given a positive answer to posible permission requests
@@ -11,8 +10,24 @@ sudo yum install git -y
 sudo yum install golang -y
 
 # npm & NodeJS installation commands
-curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
+curl -sL https://rpm.nodesource.com/setup_16.x | sudo bash -
 sudo yum install nodejs -y
 
 # Vue cli installation command
 yes | npm install -g @vue/cli
+
+# Verify installation
+echo vue --version
+
+#CLONING TESTING REPOSITORY
+git clone https://github.com/jdmendozaa/vuego-demoapp.git
+
+#Building Go environment for the backend
+cd vuego-demoapp/server
+go build
+
+# Environment variable PORT injected into the environment where the app will run
+echo 'export PORT=4001' >> /etc/profile
+
+# Move the vuego-demoapp binary file to the /shared folder as requested
+mv vuego-demoapp /shared/
