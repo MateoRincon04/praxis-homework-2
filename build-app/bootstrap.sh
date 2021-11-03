5f12dfa5c4e6cd04fc2fc7b9ce49009bfa1361ca
MateoRincon04 <marinconar@unal.edu.co>
2021-11-03 15:21:43 -0500

    trying to debug

--- build-app/bootstrap.sh
+++ build-app/bootstrap.sh
@@ -5,29 +5,33 @@
 
 # Git installation via yum command
 sudo yum install git -y
+git --version
 
 # Go installation via yum command
 sudo yum install golang -y
+go version
 
 # npm & NodeJS installation commands
-curl -sL https://rpm.nodesource.com/setup_16.x | sudo bash -
+curl -sL https://rpm.nodesource.com/setup_14.x | sudo bash -
 sudo yum install nodejs -y
 
+node --version
+npm --version
+
 # Vue cli installation command
-yes | npm install -g @vue/cli
+echo "aqui"
+# cd /
+# cd /home/vagrant/
+# touch package.json
+echo "removing package-lock"
+rm package-lock.json
+echo "removed package-lock"
+rm -rf node_modules
+echo "removing modules"
+npm config set registry https://registry.npmjs.org/
+echo "config registry"
+npm config set strict-ssl false
+echo "config strict-ssl"
+sudo npm install -g @vue/cli -y
 
 # Verify installation
-echo vue --version
-
-#CLONING TESTING REPOSITORY
-git clone https://github.com/jdmendozaa/vuego-demoapp.git
-
-#Building Go environment for the backend
-cd vuego-demoapp/server
-go build
-
-# Environment variable PORT injected into the environment where the app will run
-echo 'export PORT=4001' >> /etc/profile
-
-# Move the vuego-demoapp binary file to the /shared folder as requested
-mv vuego-demoapp /shared/
